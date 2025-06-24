8297bda6ffb498e91abdec41b91eeaa167a8d217
Parth Kotawadekar <kotawadekarparth@gmail.com>
2025-06-24 23:37:33 +0530

    Successfully Running API

--- backend/src/main/java/com/jwt/api/controller/UserManagementController.java
+++ backend/src/main/java/com/jwt/api/controller/UserManagementController.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jwt.api.dto.ReqRes;
@@ -17,6 +19,7 @@ import com.jwt.api.entity.OurUsers;
 import com.jwt.api.service.UsersManagementService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserManagementController {
 	
 	@Autowired
